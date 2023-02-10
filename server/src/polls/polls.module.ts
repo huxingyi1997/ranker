b769cf62cc3987af58bb2d241b516655e08b22a2
xingyi.hu <xingyi.hu@shopee.com>
2023-02-10 20:58:55 +0800

    feat: import redis module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PollsController } from './polls.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PollsService } from './polls.service';
+import { redisModule } from '../redis/modules.config';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, redisModule],
   controllers: [PollsController],
   providers: [PollsService],
 })
