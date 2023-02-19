9bdf55e6ebbfb8334dd29a72ce0474498da682cf
xingyi.hu <xingyi.hu@shopee.com>
2023-02-19 11:22:32 +0800

    feat: gateway initialize

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+import { SocketIOAdapter } from './socket-io-adapter';
+
 async function bootstrap() {
   const logger = new Logger('Main (main.ts)');
   const app = await NestFactory.create(AppModule);
@@ -16,6 +18,7 @@ async function bootstrap() {
       new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
     ],
   });
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
 
   const port = parseInt(configService.get('PORT'));
   await app.listen(port);
--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -4,11 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
 import { PollsRepository } from './polls.repository';
+import { PollsGateway } from './polls.gateway';
 import { jwtModule, redisModule } from '../redis/modules.config';
 
 @Module({
   imports: [ConfigModule, redisModule, jwtModule],
   controllers: [PollsController],
-  providers: [PollsService, PollsRepository],
+  providers: [PollsService, PollsRepository, PollsGateway],
 })
 export class PollsModule {}
