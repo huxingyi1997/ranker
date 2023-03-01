f5536276e5877f8eb856dd85983182f2bf7398bb
xingyi.hu <xingyi.hu@shopee.com>
2023-03-01 20:13:46 +0800

    feat: pages in front-end

---
+++ client/src/Pages.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { CSSTransition } from 'react-transition-group';
+
+import { AppPage, state } from './store';
+import Welcome from './pages/Welcome';
+import Create from './pages/Create';
+import Join from './pages/Join';
+import { useSnapshot } from 'valtio';
+
+const routeConfig = {
+  [AppPage.Welcome]: Welcome,
+  [AppPage.Create]: Create,
+  [AppPage.Join]: Join,
+};
+
+const Pages: React.FC = () => {
+  // import the state and hook
+  const currentState = useSnapshot(state);
+  return (
+    <>
+      {Object.entries(routeConfig).map(([page, Component]) => (
+        <CSSTransition
+          key={page}
+          in={page === currentState.currentPage}
+          timeout={300}
+          classNames="page"
+          unmountOnExit
+        >
+          <div className="page mobile-height max-w-screen-sm mx-auto py-8 px-4 overflow-y-auto">
+            <Component />
+          </div>
+        </CSSTransition>
+      ))}
+    </>
+  );
+};
+
+export default Pages;
